fd7ad66c6054670e4d2994b896defaee4966e0cf
Abhishek Yadav <b4skyx@gmail.com>
2020-05-30 09:18:54 +0530

    Refactor-repo

--- Sum of 2.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/two-sum/description/
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        m={}
-        for i in range(len(nums)):
-            c=target-nums[i]
-            if(c in m):
-                return [m[c],i]
-            m[nums[i]]=i
